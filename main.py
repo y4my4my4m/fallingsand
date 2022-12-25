700f7c4fa73aeab820ffbb29a34f15443d24ec9c
y4my4my4m <8145020+y4my4my4m@users.noreply.github.com>
2022-12-25 00:21:03 -0500

    player

--- main.py
+++ main.py
@@ -23,6 +23,34 @@ map_array = [[AIR for _ in range(MAP_WIDTH)] for _ in range(MAP_HEIGHT)]
 # The array that stores the update delays for each tile
 update_delay_array = [[0 for _ in range(MAP_WIDTH)] for _ in range(MAP_HEIGHT)]
 
+# The size of the character (in tiles)
+CHARACTER_WIDTH = 2
+CHARACTER_HEIGHT = 2
+
+# The initial position of the character (in tiles)
+character_x = 50
+character_y = 0
+
+# The vertical velocity of the character (in tiles per simulation step)
+character_vy = 0
+
+# The maximum vertical velocity of the character (in tiles per simulation step)
+MAX_VY = 10
+
+# The acceleration of the character when falling (in tiles per simulation step per simulation step)
+GRAVITY = 0.5
+
+# The friction of the character when sliding (in tiles per simulation step per simulation step)
+FRICTION = 0.9
+
+# The force applied to the character when jumping (in tiles per simulation step)
+JUMP_FORCE = 4
+
+# Set the character movement speed
+MOVEMENT_SPEED = 2
+
+floating_timer = 0
+
 # Initialize Pygame
 pygame.init()
 
@@ -30,11 +58,8 @@ pygame.init()
 screen = pygame.display.set_mode((MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE))
 pygame.display.set_caption('Falling Sand')
 
-# Initialize the map with some walls and water
-# for i in range(50):
-#     map_array[i][0] = WALL
-
-map_array[50][0] = WATER
+# Initialize the map a block of sand 
+# map_array[50][0] = SAND
 
 # Set the colors for each material
 colors = {
@@ -60,6 +85,8 @@ while running:
             mouse_down = False
         elif event.type == pygame.KEYDOWN:
             # Change the current material based on the key pressed
+            if event.key == pygame.K_0:
+                current_material = AIR
             if event.key == pygame.K_1:
                 current_material = WATER
             elif event.key == pygame.K_2:
@@ -72,6 +99,62 @@ while running:
         tile_x, tile_y = mouse_x // TILE_SIZE, mouse_y // TILE_SIZE
         # Place the current material on the map
         map_array[tile_x][tile_y] = current_material
+    # Check for key presses
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        character_x -= MOVEMENT_SPEED
+    if keys[pygame.K_d]:
+        character_x += MOVEMENT_SPEED
+    if keys[pygame.K_w]:
+        character_y -= JUMP_FORCE
+    if keys[pygame.K_s]:
+        character_y += MOVEMENT_SPEED
+    # Update the character position
+    character_y += character_vy
+    character_vy += GRAVITY
+    character_vy = min(character_vy, MAX_VY)
+    character_x = max(0, min(character_x, MAP_WIDTH - CHARACTER_WIDTH))
+
+    # Make sure the character is not falling off the map
+    character_y = max(0, min(character_y, MAP_HEIGHT - CHARACTER_HEIGHT))
+
+    # Check if the character is standing on solid ground
+    standing = False
+    for i in range(CHARACTER_WIDTH):
+        for j in range(CHARACTER_HEIGHT):
+            x = int(character_x) + i
+            y = int(character_y) + j
+            if 0 <= x < MAP_WIDTH and 0 <= y < MAP_HEIGHT and map_array[x][y] in (WALL, SAND):
+                standing = True
+                break
+    if standing:
+        # Reset the floating timer if the character is standing on solid ground
+        floating_timer = 0
+        character_vy = 0
+        character_y = int(character_y // CHARACTER_HEIGHT) * CHARACTER_HEIGHT
+    else:
+        # Check if the character is standing on water
+        standing_on_water = False
+        for i in range(CHARACTER_WIDTH):
+            for j in range(CHARACTER_HEIGHT):
+                x = int(character_x) + i
+                y = int(character_y) + j
+                if 0 <= x < MAP_WIDTH and 0 <= y < MAP_HEIGHT and map_array[x][y] == WATER:
+                    standing_on_water = True
+                    break
+        if standing_on_water:
+            # Float in water
+            if character_vy > 0:
+                character_vy = -0.025
+                # Reset the floating timer if the character was moving upwards
+            #     floating_timer = 30
+            # if floating_timer > 0:
+            #     # Float on top of the water
+            #     character_vy = -0.05
+            #     floating_timer -= 1
+            # else:
+            #     # Slowly sink in the water
+            #     character_vy += 0.1
     # Shuffle the order in which the tiles are processed
     tiles = [(i, j) for i in range(MAP_WIDTH) for j in range(MAP_HEIGHT)]
     random.shuffle(tiles)
@@ -112,6 +195,11 @@ while running:
     for i in range(MAP_WIDTH):
         for j in range(MAP_HEIGHT):
             pygame.draw.rect(screen, colors[map_array[i][j]], pygame.Rect(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+
+   # Draw the character
+    for i in range(CHARACTER_WIDTH):
+        for j in range(CHARACTER_HEIGHT):
+            pygame.draw.rect(screen, (255, 0, 0), (character_x * TILE_SIZE + i * TILE_SIZE, character_y * TILE_SIZE + j * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
     # Update the display
     pygame.display.flip()
